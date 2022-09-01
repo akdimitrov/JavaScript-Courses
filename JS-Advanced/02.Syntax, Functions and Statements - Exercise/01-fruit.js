3b285597ec73f3960374aee143310823f1209702
Aleksandar Dimitrov <alexkdimitrov@gmail.com>
2022-09-01 23:27:12 +0300

    Added problem solution

---
+++ JS-Advanced/02.Syntax, Functions and Statements - Exercise/01-fruit.js
@@ -0,0 +1,8 @@
+function solve(fruit, weight, price) {
+    let kilograms = weight / 1000;
+    let money = price * kilograms;
+    console.log(`I need ${money.toFixed(2)} to buy ${kilograms.toFixed(2)} kilograms ${fruit}.`);
+}
+
+solve('orange', 2500, 1.80);
+solve('apple', 1563, 2.35);

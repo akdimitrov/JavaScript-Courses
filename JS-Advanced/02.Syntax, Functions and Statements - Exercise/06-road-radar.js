e2e1a2c436cffb906f7adecce62297c52b1727ad
Aleksandar Dimitrov <alexkdimitrov@gmail.com>
2022-09-03 02:46:10 +0300

    Added problem solution

---
+++ JS-Advanced/02.Syntax, Functions and Statements - Exercise/06-road-radar.js
@@ -0,0 +1,30 @@
+function solve(speed, area) {
+    let speedLimit = 0;
+    switch (area) {
+        case 'motorway': speedLimit = 130; break;
+        case 'interstate': speedLimit = 90; break;
+        case 'city': speedLimit = 50; break;
+        case 'residential': speedLimit = 20; break;
+    }
+
+    if (speed <= speedLimit) {
+        console.log(`Driving ${speed} km/h in a ${speedLimit} zone`);
+    } else {
+        let diff = speed - speedLimit;
+        let status;
+        if (diff <= 20) {
+            status = 'speeding';
+        } else if (diff <= 40) {
+            status = 'excessive speeding';
+        } else {
+            status = 'reckless driving';
+        }
+
+        console.log(`The speed is ${diff} km/h faster than the allowed speed of ${speedLimit} - ${status}`);
+    }
+}
+
+solve(40, 'city');
+solve(21, 'residential');
+solve(120, 'interstate');
+solve(200, 'motorway');

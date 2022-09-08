32fa6a2a7592aefe39d7933b7bc9cada70c5952e
Aleksandar Dimitrov <alexkdimitrov@gmail.com>
2022-09-09 00:12:58 +0300

    Added problem solution

---
+++ JS-Advanced/03.Arrays and Nested Arrays - Lab/07-piece-of-pie.js
@@ -0,0 +1,23 @@
+function solve(flavors, target1, target2) {
+    return flavors.slice(flavors.indexOf(target1), flavors.indexOf(target2) + 1);
+}
+
+console.log(solve(
+    ['Pumpkin Pie',
+    'Key Lime Pie',
+    'Cherry Pie',
+    'Lemon Meringue Pie',
+    'Sugar Cream Pie'],
+    'Key Lime Pie',
+    'Lemon Meringue Pie'
+));
+console.log(solve(
+    ['Apple Crisp',
+    'Mississippi Mud Pie',
+    'Pot Pie',
+    'Steak and Cheese Pie',
+    'Butter Chicken Pie',
+    'Smoked Fish Pie'],
+    'Pot Pie',
+    'Smoked Fish Pie'
+));

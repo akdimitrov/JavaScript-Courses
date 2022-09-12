45facd4b43066662db81f66521b34ca3d18f2bbd
Aleksandar Dimitrov <alexkdimitrov@gmail.com>
2022-09-13 01:24:22 +0300

    Modified solution

--- JS-Advanced/02.Syntax, Functions and Statements - Exercise/04-previous-day.js
+++ JS-Advanced/02.Syntax, Functions and Statements - Exercise/04-previous-day.js
@@ -1,7 +1,7 @@
 function solve(year, month, day) {
-    let date = new Date(year, month, day);
+    let date = new Date(`${year}-${month}-${day}`);
     date.setDate(date.getDate() - 1);
-    console.log(`${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`);
+    console.log(`${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`);
 }
 
 solve(2016, 9, 30);

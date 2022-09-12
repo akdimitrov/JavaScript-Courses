0bb8c9937a6653de1587567d9dc6ff68a403c25a
Aleksandar Dimitrov <alexkdimitrov@gmail.com>
2022-09-12 20:27:02 +0300

    Added problem solution

---
+++ JS-Advanced/03.Arrays and Nested Arrays - Lab/11-equal-neighbors.js
@@ -0,0 +1,32 @@
+function solve(matrix) {
+    let pairs = 0;
+    for (let row = 0; row < matrix.length; row++) {
+        for (let col = 0; col < matrix[row].length; col++) {
+            if (row < matrix.length - 1 && matrix[row][col] === matrix[row + 1][col]){
+                pairs++;
+            }
+            if (col < matrix[row].length - 1 && matrix[row][col] === matrix[row][col + 1]) {
+                pairs++;
+            }
+        }
+    }
+
+    return pairs;
+}
+
+console.log(solve([
+    ['2', '3', '4', '7', '0'],
+    ['4', '0', '5', '3', '4'],
+    ['2', '3', '5', '4', '2'],
+    ['9', '8', '7', '5', '4']
+]));
+console.log(solve([
+    ['test', 'yes', 'yo', 'ho'],
+    ['well', 'done', 'yo', '6'],
+    ['not', 'done', 'yet', '5']
+]));
+console.log(solve([
+    [2, 2, 5, 7, 4],
+    [4, 0, 5, 3, 4],
+    [2, 5, 5, 4, 2]
+]));
